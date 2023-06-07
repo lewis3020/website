bc2759dcc4f0c1ae373e15681281c9d563d46789
lewis <lewisaquinosanchez@gmail.com>
2023-06-06 22:09:12 -0400

    almost final version

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function showOutput() {
     var output = document.getElementById("output");
-    output.innerHTML = "Selected options: ";
+    output.innerHTML = "Problems: ";
 
     var checkboxes = document.querySelectorAll("input[type=checkbox]:checked");
     for (var i = 0; i < checkboxes.length; i++) {
@@ -8,14 +8,3 @@ function showOutput() {
     }
     output.innerHTML = output.innerHTML.slice(0, -2);
 }
-function reset(){
-    //uncheck boxes, scroll to top
-    var boxes = document.querySelectorAll('input[type="checkbox"]')
-    for(var checkbox of boxes)
-        checkbox.checked=false;
-    document.getElementById("output").innerHTML="";
-    document.body.scrollTop = 0; //For Safari
-    document.documentElement.scrollTop = 0; //For Chrome, Firefox, IE, and Opera
-}
-//v4.1
-//Update Feb 22nd, 2023
